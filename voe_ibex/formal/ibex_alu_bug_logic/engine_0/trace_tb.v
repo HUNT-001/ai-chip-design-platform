384232621384344c259c49e7de0ead764b6c4c48
HUNT-001 <tanushpavan2006@gmail.com>
2026-08-07 14:31:48 +0530

    updated the ibex alu bug logic verilog file

--- voe_ibex/formal/ibex_alu_bug_logic/engine_0/trace_tb.v
+++ voe_ibex/formal/ibex_alu_bug_logic/engine_0/trace_tb.v
@@ -8,15 +8,15 @@ module testbench(input clock, output reg genclock);
 `endif
   reg genclock = 1;
   reg [31:0] cycle = 0;
-  reg [31:0] PI_a;
   reg [31:0] PI_b;
-  reg [6:0] PI_op;
   wire [0:0] PI_clk = clock;
+  reg [6:0] PI_op;
+  reg [31:0] PI_a;
   ibex_alu_fv UUT (
-    .a(PI_a),
     .b(PI_b),
+    .clk(PI_clk),
     .op(PI_op),
-    .clk(PI_clk)
+    .a(PI_a)
   );
 `ifndef VERILATOR
   initial begin
@@ -35,22 +35,22 @@ module testbench(input clock, output reg genclock);
 `ifndef VERILATOR
     #1;
 `endif
-    // UUT.$auto$async2sync.\cc:107:execute$167  = 1'b0;
-    // UUT.$auto$async2sync.\cc:116:execute$171  = 1'b1;
-    UUT.dut.$auto$proc_rom.\cc:155:do_switch$95 [6'b000010] = 1'b0;
-    UUT.dut.$auto$proc_rom.\cc:155:do_switch$95 [6'b000100] = 1'b0;
+    // UUT.$auto$async2sync.\cc:107:execute$215  = 1'b0;
+    // UUT.$auto$async2sync.\cc:116:execute$219  = 1'b1;
+    UUT.dut.$auto$proc_rom.\cc:155:do_switch$143 [6'b000010] = 1'b0;
+    UUT.dut.$auto$proc_rom.\cc:155:do_switch$143 [6'b000011] = 1'b0;
 
     // state 0
-    PI_a = 32'b11001010111111101111000000001101;
-    PI_b = 32'b00000000000011011100111000101001;
+    PI_b = 32'b00110110010000010000011111110010;
     PI_op = 7'b0000010;
+    PI_a = 32'b11001010111111101111000000001101;
   end
   always @(posedge clock) begin
     // state 1
     if (cycle == 0) begin
-      PI_a <= 32'b10101000000101010100011101010111;
-      PI_b <= 32'b00000001101000000000011010000011;
-      PI_op <= 7'b0000100;
+      PI_b <= 32'b00000000101000000100110000000101;
+      PI_op <= 7'b0000011;
+      PI_a <= 32'b10011100000001101010000000110010;
     end
 
     genclock <= cycle < 1;
