db9e6a3fd5b36cbc829ac25245569ade225161f6
HUNT-001 <tanushpavan2006@gmail.com>
2026-08-31 14:01:26 +0530

    test(formal): add counterexample traces for Ibex ALU logic bug

--- voe_ibex/formal/ibex_alu_bug_logic/engine_0/trace_tb.v
+++ voe_ibex/formal/ibex_alu_bug_logic/engine_0/trace_tb.v
@@ -8,15 +8,15 @@ module testbench(input clock, output reg genclock);
 `endif
   reg genclock = 1;
   reg [31:0] cycle = 0;
-  reg [31:0] PI_a;
-  reg [31:0] PI_b;
   wire [0:0] PI_clk = clock;
   reg [6:0] PI_op;
+  reg [31:0] PI_a;
+  reg [31:0] PI_b;
   ibex_alu_fv UUT (
-    .a(PI_a),
-    .b(PI_b),
     .clk(PI_clk),
-    .op(PI_op)
+    .op(PI_op),
+    .a(PI_a),
+    .b(PI_b)
   );
 `ifndef VERILATOR
   initial begin
@@ -41,16 +41,16 @@ module testbench(input clock, output reg genclock);
     UUT.dut.$auto$proc_rom.\cc:155:do_switch$143 [6'b000011] = 1'b0;
 
     // state 0
+    PI_op = 7'b0000010;
     PI_a = 32'b11001010111111101111000000001101;
     PI_b = 32'b00110110010000010000011111110010;
-    PI_op = 7'b0000010;
   end
   always @(posedge clock) begin
     // state 1
     if (cycle == 0) begin
+      PI_op <= 7'b0000011;
       PI_a <= 32'b10011100000001101010000000110010;
       PI_b <= 32'b00000000101000000100110000000101;
-      PI_op <= 7'b0000011;
     end
 
     genclock <= cycle < 1;
